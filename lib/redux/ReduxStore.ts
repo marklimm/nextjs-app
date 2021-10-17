ee802e7451702693ec1a74a28c4bfc6dcb20f4bd
Mark <mark.limm@proinbox.com>
2021-10-16 21:34:56 -0400

    attempt to resolve build error

--- lib/redux/ReduxStore.ts
+++ lib/redux/ReduxStore.ts
@@ -12,7 +12,7 @@ const ReduxStore = configureStore({
 })
 
 //  following https://react-redux.js.org/using-react-redux/usage-with-typescript#define-typed-hooks
-export type IReduxStore = ReturnType<typeof ReduxStore.getState>
+export type IReduxStore = ReturnType<typeof reducer>
 export type AppDispatch = typeof ReduxStore.dispatch
 
 export default ReduxStore
